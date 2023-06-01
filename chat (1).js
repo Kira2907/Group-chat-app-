436f11d9ccb08e7f1ea80a2b11f58e7d893207e3
Kira2907 <130835235+Kira2907@users.noreply.github.com>
2023-06-01 15:35:49 +0530

    Login backend APIs
    Created backend api which sends jwt token in response to frontend after successful login.

---
+++ chat (1).js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const usersController = require('../controllers/User');
+const router = express.Router();
+
+router.get('/sign-up', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'sign-up.html'));
+ });
+
+router.post('/signup', usersController.signup);
+
+router.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'login.html'));
+ });
+
+router.post('/login', usersController.login);
+ 
+module.exports = router;
